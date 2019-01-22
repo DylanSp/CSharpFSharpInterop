6c2ee45b564f091b0be15f48c27174cdf85d5261
Dylan Sprague <dylan.richard.sprague@gmail.com>
2019-01-21 19:35:07 -0500

    Add project files.

---
+++ CSharpFSharpInterop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpFSharpInterop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
